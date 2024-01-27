b6e16f59ef40478844881d299af53d82f93852f6
Shema Dabiri <dabirishema@gmail.com>
2024-01-27 15:52:35 -0600

    finance controller

--- server/controllers/finance-controller.js
+++ server/controllers/finance-controller.js
@@ -1,29 +1,57 @@
 const Finance = require('../models/finance');
+const User = require('../models/users')
 const finances = {
-    createFinance: async(req, res)=>{
+    createFinance: async (req, res) => {
         const id = req.body.id;
-        const newProfile = new Finance({
-            user_id: id,
-            credit: 0,
-            debit: 0,
-        });
-        newProfile.save();
+        try {
+            const existingFinance = await Finance.findOne({ user_id: id });
+            const existingUser = await User.findOne({_id: id})
+            if (!existingFinance && existingUser) {
+                const newProfile = new Finance({
+                    user_id: id,
+                    credit: 0,
+                    debit: 0,
+                    totalExpenditure: 0
+                });
+                newProfile.save();
+                res.json({ status: 200, finance: newProfile })
+            }
+            else {
+                res.json({ status: 400, error: 'user for this finance profile does not exist or there is already a profile' })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+       
+       
     },
-    updateFinance: async(req, res)=>{
+    updateFinance: async (req, res) => {
         const id = req.body.id;
         const credit = req.body.credit;
         const debit = req.body.debit;
-
-        try{
-            await Finance.updateOne({user_id: is}, $set{
-                
-            })
-        }catch(e){
-            console.log(e)
+        const expenditure = req.body.expenditure;
+        const existing = await Finance.findOne({ user_id: id });
+        if (existing) {
+            try {
+                await Finance.updateOne({ user_id: id }, { $set: { credit: credit, debit: debit, totalExpenditure: expenditure } })
+                res.json({ status: 200, finance: existing })
+            } catch (e) {
+                console.log(e)
+            }
         }
-    },
-    deleteFinance: async(req, res)=>{
+        else {
+            res.json({ status: 400, error: 'user for this finance profile does not exist' })
+        }
 
+    },
+    deleteFinance: async (req, res) => {
+        const id = req.body.id;
+        try {
+            await Finance.deleteOne({ user_id: id })
+            res.json({ status: 200, success: 'finance profile successfully deleted' })
+        } catch (error) {
+            console.log(e);
+        }
     }
 }
 
