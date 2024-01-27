e6a425bebda1d7734738975ea1d65a0265613923
Shema Dabiri <dabirishema@gmail.com>
2024-01-27 14:17:10 -0600

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,23 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
-  return (
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#051923' }}>
-        <a className="navbar-brand" href="/">
-          Your Logo or Brand
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item active">
-              <a className="nav-link" href="/Login.js">
-                Login
-              </a>
-            </li>
-            {/* Add more navigation items as needed */}
-          </ul>
-        </div>
-      </nav>
 
-      <header className="App-header p-5 text-center">
-        <h1 className="display-4">Welcome to Our Home Page!</h1>
-        <p className="lead">
-          Our mission is to create a supportive and accessible space where adults with intellectual and/or cognitive disabilities can thrive and live their lives to the fullest. Here, you'll find a community dedicated to understanding and embracing the unique strengths of every individual. Explore the resources and discover opportunities that empower you to lead a fulfilling life.
-        </p>
-      </header>
-    </div>
+ 
+  return (
+    <>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
